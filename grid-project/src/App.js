ecef52d1744f4c5df11a13db2c3c10b418bb0c2d
Skye Flowers <skyeflowers@Skyes-MacBook-Pro.local>
2024-08-21 00:33:30 -0500

    updated api grid

---
+++ grid-project/src/App.js
@@ -0,0 +1,14 @@
+import Table from './table';
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Table</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ grid-project/src/table.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+function Table() {
+const [data, setData] = useState([]);
+
+useEffect(() => {
+    fetch("https://reqres.in/api/users")
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Network response unsuccessful');
+        }
+    })
+    .then(data => {
+        console.log(data);
+        setData(data.data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    })
+  })
+
+    return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Picture</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(user => (
+            <tr>
+              <td>{user.first_name} {user.last_name}</td>
+              <td><img src={user.avatar} width="100"/></td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
