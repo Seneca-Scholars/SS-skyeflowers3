a5eba4ea391c5bd513bcf588168f47fe9d2a37bc
Skye Flowers <skyeflowers@Skyes-MacBook-Pro.local>
2024-08-26 15:10:23 -0500

    added nonfunc form

--- front-end/src/table.jsx
+++ front-end/src/table.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 function Table() {
 const [data, setData] = useState([]);
+const [formData, setFormData] = useState({
+  id: null, 
+  name: '', 
+});
 
 useEffect(() => {
     fetch("/api/items")
@@ -18,10 +22,87 @@ useEffect(() => {
     .catch(error => {
       console.error('Error fetching data:', error);
     })
-  },[])
+  },[]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevFormData => ({
+      ...prevFormData, 
+      [name]: value
+    }));
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+   
+    if (formData.id) {
+      fetch(`/api/items/${formData.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: formData.name,
+        }), 
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(updatedItem => {
+        setData(prevData => prevData.map(item => item.id === updatedItem.id ? updatedItem : item));
+        setFormData({ id: null, name: '', phone: '', address: '' });
+      })
+      .catch(error => {
+        console.error('Error updating data:', error);
+      });
+    } else {
+      fetch('/api/items', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({
+          name: formData.name,
+        }),
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(newItem => {
+        setData(prevData => [...prevData, newItem]);
+        setFormData({ id: null, name: '' });
+      })
+      .catch(error => {
+        console.error('Error posting data:', error);
+      });
+    }
+  };
 
     return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input 
+              type='text' 
+              name='name' 
+              onChange={handleChange}
+              value={formData.name} 
+              required
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
       <table>
         <thead>
           <tr>
